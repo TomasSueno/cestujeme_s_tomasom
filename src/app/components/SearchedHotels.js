01f87dffe7426e768034dbd6de121a17fa5b5a03
TomasSueno <sleboda2008@gmail.com>
2025-10-25 14:27:39 +0200

    .

--- src/app/components/SearchedHotels.js
+++ src/app/components/SearchedHotels.js
@@ -7,22 +7,22 @@ import { useEffect, useState } from "react"
 export default function SearchedHotels() {
     const searchParams = useSearchParams()
 
-    const place = searchParams.get("place")
-    const startDate = searchParams.get("startDate")
-    const endDate = searchParams.get("endDate")
-    const adultsCount = searchParams.get("adultsCount")
-    const childCount = searchParams.get("childCount")
-    const childAge = searchParams.get("childAge")
+    // const place = searchParams.get("place")
+    // const startDate = searchParams.get("startDate")
+    // const endDate = searchParams.get("endDate")
+    // const adultsCount = searchParams.get("adultsCount")
+    // const childCount = searchParams.get("childCount")
+    // const childAge = searchParams.get("childAge")
 
     return (
         <>
-            <p>{place}</p>
+            {/* <p>{place}</p>
             <p>{startDate}</p>
             <p>{endDate}</p>
             <p>{adultsCount}</p>
             <p>{childCount}</p>
             <p>{childAge}</p> 
-            <Link href="/">Lets find the hotel with these parameters</Link>          
+            <Link href="/">Lets find the hotel with these parameters</Link>           */}
         </>
     )
 }
